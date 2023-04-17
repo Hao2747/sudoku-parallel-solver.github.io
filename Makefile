612ae635cc7e19ada1f253ec58f2c5bbd66b34f1
Hao2747 <52426444+Hao2747@users.noreply.github.com>
2023-04-17 15:13:31 -0400

    Modularize Files
    * create util.h
    
    * add makefile
    
    * put in src folder
    
    * complete modularization
    
    * solve more than one puzzle
    
    * set header guard in grid.h, remove unnecessary header
    
    * Get rid of excessive guard
    
    ---------
    
    Co-authored-by: kzhu2 <99935631+kzhu2@users.noreply.github.com>

---
+++ Makefile
@@ -0,0 +1,39 @@
+OUTPUTDIR := bin/
+
+CFLAGS := -std=c++14 -fvisibility=hidden -lpthread
+
+ifeq (,$(CONFIGURATION))
+	CONFIGURATION := release
+endif
+
+ifeq (debug,$(CONFIGURATION))
+CFLAGS += -g
+else
+CFLAGS += -O2 -fopenmp
+endif
+
+SOURCES := src/*.cpp
+HEADERS := src/*.h
+
+TARGETBIN := solver-$(CONFIGURATION)
+
+.SUFFIXES:
+.PHONY: all clean
+
+all: $(TARGETBIN)
+
+$(TARGETBIN): $(SOURCES) $(HEADERS)
+	$(CXX) -o $@ $(CFLAGS) $(SOURCES) 
+
+format:
+	clang-format -i src/*.cpp src/*.h
+
+clean:
+	rm -rf ./solver-$(CONFIGURATION)
+
+
+FILES = src/*.cpp \
+		src/*.h
+
+handin.tar: $(FILES)
+	tar cvf handin.tar $(FILES)
---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stdbool.h>
+#include <vector>
+#include <tuple>
+
+
+#include "backsolve.cpp"
+#include "bfs.cpp"
+#include "util.h"
+
+
+
+typedef int dtype; 
+
+
+
+
+
+class Solver {
+public:
+  virtual Grid seq_solve(Grid g) = 0;
+  virtual Grid par_solve(Grid g) = 0;
+  
+};
+
+
+
+
+int main()
+{
+    std::vector<Grid> all_grids;
+    // file_to_grids("testbench/medium-100", all_grids);
+    file_to_grids("testbench/simple-500", all_grids);
+
+    BFS b;
+    for (Grid grid: all_grids){
+      Grid f = b.seq_solve(grid); 
+      int ans = f.validate();
+      if (ans == false){
+      std::cout << "This puzzle is solved incorrectly" << std::endl;
+
+      }
+    }
+    
+
+    
+    
+    
+    
+    return 0;
+}
