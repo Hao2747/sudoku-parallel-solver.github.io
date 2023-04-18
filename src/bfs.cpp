bc4c303690d3ea9dd0c0139106b3e62ce1dd94f6
Hao Zhang <zhanghao.jerry@gmail.com>
2023-04-17 22:22:45 -0400

    attempt to parallel bfs. not tested. coord barrier

--- src/bfs.cpp
+++ src/bfs.cpp
@@ -3,97 +3,124 @@
 
 #include "grid.h"
 
-class BFS {
-  public:
-  static Grid seq_solve(Grid g) {
-    std::vector<Coordinate> coords = g.find_all_empty_cells();
-    
-    std::deque<Grid> possible;
-    std::deque<Grid> next_iter;
-    
-    possible.push_back(g);
-    
-    //we will do the bfs solving by choosing one of the unsolved squres
-    //then we will find all possibilies for that square
-    //at any particular timestep, the coords is all the same
-    //pick and solve exactly one square in every full iteration of the while loop
-    while(!coords.empty()) {
-        Coordinate c = coords.back();
-        coords.pop_back();
-        next_iter.clear();
-        
-        while(!possible.empty()) {
-            Grid current = possible.front();
-            possible.pop_front();
-            
-            for(dtype guess = 1; guess <= g.size(); guess++) {
-                //overriding value here (should be ok)
-                current[c.r][c.c] = guess;
-                
-                //this function is not efficient and should be replaced
-                //Instead this function will be replaced by taking the current loc
-                //the row/col/and box of the location of the guess and check if its possible
-                if(current.is_possible()) {
-                    next_iter.push_back(current);
+class BFS
+{
+public:
+    static Grid seq_solve(Grid g)
+    {
+        std::vector<Coordinate> coords = g.find_all_empty_cells();
+
+        std::deque<Grid> possible;
+        std::deque<Grid> next_iter;
+
+        possible.push_back(g);
+
+        // we will do the bfs solving by choosing one of the unsolved squres
+        // then we will find all possibilies for that square
+        // at any particular timestep, the coords is all the same
+        // pick and solve exactly one square in every full iteration of the while loop
+        while (!coords.empty())
+        {
+            Coordinate c = coords.back();
+            coords.pop_back();
+            next_iter.clear();
+
+            while (!possible.empty())
+            {
+                Grid current = possible.front();
+                possible.pop_front();
+
+                for (dtype guess = 1; guess <= g.size(); guess++)
+                {
+                    // overriding value here (should be ok)
+                    current[c.r][c.c] = guess;
+
+                    // this function is not efficient and should be replaced
+                    // Instead this function will be replaced by taking the current loc
+                    // the row/col/and box of the location of the guess and check if its possible
+                    if (current.is_possible())
+                    {
+                        next_iter.push_back(current);
+                    }
+                }
+            }
+
+            possible.swap(next_iter);
+        }
+
+        if (!possible.empty())
+        {
+            return possible[0];
+        }
+
+        return Grid();
+    }
+
+    static Grid par_solve(Grid g)
+    {
+        std::vector<Coordinate> coords = g.find_all_empty_cells();
+
+        std::deque<Grid> possible;
+        std::deque<Grid> next_iter;
+
+        possible.push_back(g);
+
+        // we will do the bfs solving by choosing one of the unsolved squres
+        // then we will find all possibilies for that square
+        // at any particular timestep, the coords is all the same
+        // pick and solve exactly one square in every full iteration of the while loop
+        while (!coords.empty())
+        {
+            Coordinate c = coords.back();
+            coords.pop_back();
+            next_iter.clear();
+
+#pragma omp parallel default(none) shared(c) firstprivate(next_iter)
+            {
+                while (true)
+                {
+#pragma omp critical (deque_front)
+                    {
+                        if (!possible.empty())
+                        {
+                            Grid current = possible.front();
+                            possible.pop_front();
+                        }
+                        else{
+                            break;
+                        }
+                        
+                    }
+                    
+                   
+
+                    for (dtype guess = 1; guess <= g.size(); guess++)
+                    {
+                        // overriding value here (should be ok)
+                        current[c.r][c.c] = guess;
+
+                        // this function is not efficient and should be replaced
+                        // Instead this function will be replaced by taking the current loc
+                        // the row/col/and box of the location of the guess and check if its possible
+                        if (current.is_possible())
+                        {
+                            next_iter.push_back(current);
+                        }
+                    }
+                }
+
+                #pragma omp critical (deque_end)
+                {
+                    possible.insert(possible.end(), next_iter.begin(), next_iter.end());
                 }
             }
         }
-        
-        possible.swap(next_iter);
-    }
-    
-    if(!possible.empty()) {
-        return possible[0];
-    } 
-        
-    return Grid();
-    
-  }
-  
-  static Grid par_solve(Grid g) {
-      
-     std::vector<Coordinate> coords = g.find_all_empty_cells_parallel();
-    
-    std::deque<Grid> possible;
-    std::deque<Grid> next_iter;
-    
-    possible.push_back(g);
-    
-    //we will do the bfs solving by choosing one of the unsolved squres
-    //then we will find all possibilies for that square
-    //at any particular timestep, the coords is all the same
-    //pick and solve exactly one square in every full iteration of the while loop
-    while(!coords.empty()) {
-        Coordinate c = coords.back();
-        coords.pop_back();
-        next_iter.clear();
-        
-        while(!possible.empty()) {
-            Grid current = possible.front();
-            possible.pop_front();
-            
-            for(dtype guess = 1; guess <= g.size(); guess++) {
-                //overriding value here (should be ok)
-                current[c.r][c.c] = guess;
-                
-                //this function is not efficient and should be replaced
-                //Instead this function will be replaced by taking the current loc
-                //the row/col/and box of the location of the guess and check if its possible
-                if(current.is_possible()) {
-                    next_iter.push_back(current);
-                }
-            }
+
+        if (!possible.empty())
+        {
+            return possible[0];
         }
-        
-        possible.swap(next_iter);
+
+        return Grid();
     }
-    
-    if(!possible.empty()) {
-        return possible[0];
-    } 
-        
-    return Grid();
-    
-  }
-    
-};
+}
