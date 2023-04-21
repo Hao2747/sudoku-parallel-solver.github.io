97c1afdf634b1e51fb74996c8af24c1df30f224a
Hao Zhang <zhanghao.jerry@gmail.com>
2023-04-20 21:01:30 -0400

    cleanup comments

--- src/main.cpp
+++ src/main.cpp
@@ -15,39 +15,27 @@ int main(int argc, const char **argv)
   StartupOptions options = parseOptions(argc, argv);
   std::string input_file = "testbench/" + options.input_file;
   file_to_grids(input_file, all_grids);
-  // file_to_grids("testbench/simple-500", all_grids);
-  // file_to_grids("testbench/4x4", all_grids);
 
-  // Grid a(4, input);
-  // a.display_values();
   solver = options.solver;
 
   Timer t;
   t.reset();
-  // for (Grid grid: all_grids){
-  //   solved_grid = b.par_solve(grid);
-  //   // solved_grid = b.seq_solve(grid);
-  //   int ans = f.validate();
-  //   if (ans == false){
-  //     std::cout << "This puzzle is solved incorrectly" << std::endl;
 
-  //   }
-  // }
   Grid solved_grid;
-  for (Grid grid: all_grids){
+  for (Grid grid : all_grids)
+  {
     if (options.run_parallel)
-  {
-    solved_grid = solver->par_solve(grid);
-  }
-  else
-  {
-    solved_grid = solver->seq_solve(grid);
-  }
-    // solved_grid = b.seq_solve(grid);
+    {
+      solved_grid = solver->par_solve(grid);
+    }
+    else
+    {
+      solved_grid = solver->seq_solve(grid);
+    }
     int ans = solved_grid.validate();
-    if (ans == false){
+    if (ans == false)
+    {
       std::cout << "This puzzle is solved incorrectly" << std::endl;
-
     }
   }
 
