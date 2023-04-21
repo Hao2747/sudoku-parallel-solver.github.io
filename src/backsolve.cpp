c4533f5db959c0c29f01c2b97854094e0ceec308
Hao Zhang <zhanghao.jerry@gmail.com>
2023-04-21 17:22:05 -0400

    backsolve-par with task

--- src/backsolve.cpp
+++ src/backsolve.cpp
@@ -35,6 +35,42 @@ private:
     return std::make_tuple(g,false);
   }
 
+
+
+  std::tuple<Grid,bool>  recur_helper_par(Grid g, std::vector<Coordinate> coords)
+  {
+    // g.display_values();
+    if (coords.empty())
+    {
+      return std::make_tuple(g,true);
+    }
+
+    Coordinate c = coords.back();
+    coords.pop_back();
+
+    std::tuple<Grid, bool> ret;
+    for (dtype guess = 1; guess <= g.size(); guess++)
+    {
+      g[c.r][c.c] = guess;
+      if (g.is_possible())
+      {
+        #pragma omp task shared(ret)
+        
+        ret = recur_helper(g, coords);
+        // Check if puzzle is solved completely
+        if (std::get<1>(ret))
+        {
+          return ret;
+        }
+        
+      }
+    }
+
+    //if none of approach found
+    g[c.r][c.c] = -1;
+    return std::make_tuple(g,false);
+  }
+
 public:
   Grid seq_solve(Grid g) override
   {
@@ -43,8 +79,7 @@ public:
   }
   Grid par_solve(Grid g) override
   {
-
-    // implement this later after BFS for checkpoint
-    return Grid();
+std::vector<Coordinate> coords = g.find_all_empty_cells();
+    return std::get<0>(recur_helper_par(g, coords));
   }
 };
