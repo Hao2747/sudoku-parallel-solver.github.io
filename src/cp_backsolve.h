03a5ae441ffa50b6820b3b5ff20c0825b1950f91
Hao Zhang <zhanghao.jerry@gmail.com>
2023-04-28 17:13:38 -0400

    CP backsolve parallel complete

--- src/cp_backsolve.h
+++ src/cp_backsolve.h
@@ -31,12 +31,6 @@ private:
         available_choices.push_back(i+1); // Add the index to the vector
       }
     }
-    // std::cout << "coordinate " << row << ", " << col <<" :";
-    // for (int i = 0; i < available_choices.size(); i++)
-    // {
-    //   std::cout << available_choices[i];
-    // }
-    // std::cout << std::endl;
 
     Grid private_g = g;
     for (dtype guess :available_choices)
@@ -56,8 +50,7 @@ private:
   }
   bool recur_helper_par(Grid &g)
   {
-
-    int row, col;
+int row, col;
 
     // If cannot find more empty cell, puzzle is solved
     // If can find, set row and col of first empty cell
@@ -69,22 +62,35 @@ private:
 
 #pragma omp parallel shared(found_solution)
     {
-      Grid private_g = g;
-      // #pragma omp cancellation point parallel
-      for (dtype guess = 1; guess <= g.size(); guess++)
+
+    std::vector<int> available_choices; // Initialize an empty vector to store indices of 1 bits
+    std::bitset<BIT_CNT> square_choices = g[row][col].get_choices();
+    // Traverse all bits where the bit is set to 1
+    for (int i = 0; i < square_choices.size(); i++)
+    {
+      if (square_choices.test(i))
       {
-        private_g[row][col] = guess;
-        if (private_g.is_possible())
+        available_choices.push_back(i+1); // Add the index to the vector
+      }
+    }
+
+    Grid private_g = g;
+      // #pragma omp cancellation point parallel
+    for (dtype guess :available_choices)
+    {
+      private_g[row][col] = guess;
+      if (private_g.is_possible())
+      {
+        if (recur_helper(private_g))
         {
-          if (recur_helper(private_g))
-          {
-            g = private_g;
-            found_solution = true;
-            // #pragma omp cancel parallel
-          }
+          g = private_g;
+          found_solution = true;
+                      // #pragma omp cancel parallel
+
         }
       }
     }
+}
     return found_solution;
   }
 
@@ -97,14 +103,13 @@ public:
   {
     // std::vector<Coordinate> coords = g.find_all_empty_cells();
     g.set_square_choices();
-    g.print_arrays();
-
     recur_helper(g);
     return g;
   }
 
   Grid par_solve(Grid g) override
   {
+    g.set_square_choices();
     recur_helper(g);
     return g;
   }
