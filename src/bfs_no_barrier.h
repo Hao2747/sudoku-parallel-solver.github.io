22823aae09fb1a81732e9b25e099e05d86013a8c
Hao Zhang <zhanghao.jerry@gmail.com>
2023-05-01 20:01:57 -0400

    cleanup comments

--- src/bfs_no_barrier.h
+++ src/bfs_no_barrier.h
@@ -71,32 +71,27 @@ public:
         int row, col;
         dtype guess;
         std::deque<Grid> possible_grids;
-        int grid_size = g.size();
-        bool get_grid;
+        const int grid_size = g.size();
+        // bool get_grid;
         bool found_solution = false;
         omp_set_num_threads(4);
         grid_queue.push_back(g);
         omp_lock_t queue_lock;
         omp_init_lock(&queue_lock);
-int next_ticket = 0; // next ticket number to be issued
             int now_serving = 0; // current ticket being served
 // g.display_values();
-#pragma omp parallel shared(g, grid_size, grid_queue, found_solution, now_serving, next_ticket) private(private_g, row, col, possible_grids, get_grid)
-        {
+            // bool status[grid_size] = {false};
+            // status[0] = true;
+            // std::vector<bool> status(grid_size);
+#pragma omp parallel shared(g, grid_queue, found_solution, now_serving) private(private_g, row, col, possible_grids)
+            {
             int tid = omp_get_thread_num();
-            int nid = omp_get_num_threads();
-            
-
-
-            
+            int nid = omp_get_num_threads();         
             while (!found_solution)
             {
-                // std::cout << "tid " << tid << ": still running" << std::endl;
 
-                get_grid = false;
-                // #pragma omp cancellation point parallel
 
-                while (tid != now_serving % nid || grid_queue.empty())
+                while (tid != now_serving  || grid_queue.empty())
                 {
                     
 #pragma omp atomic read
@@ -113,7 +108,6 @@ int next_ticket = 0; // next ticket number to be issued
                 {
                     private_g = grid_queue.front();
                     grid_queue.pop_front();
-                    get_grid = true;
                     // std::cout << "tid " << tid << ": get from queue" << std::endl;
                     // private_g.display_values();
                     // std::cout << "only " << grid_queue.size() << " puzzles left in the queue" << std::endl;
@@ -121,16 +115,14 @@ int next_ticket = 0; // next ticket number to be issued
                 //TODO: take it out of lock
                 #pragma omp atomic update
                 now_serving++;
-                #pragma omp flush(now_serving)
+                if(now_serving == nid){
+                    now_serving = 0;
+                }
+#pragma omp flush(now_serving)
                 omp_unset_lock(&queue_lock);
 
-                // std::cout << "tid " << tid << ": release the lock" << std::endl;
 
-                if (!get_grid)
-                {
-                    continue; // could use while loop to wait until
-                }
-
+                
                 // if there is still empty cell, set row and col
                 if (!private_g.find_next_empty_cell(row, col))
                 {
@@ -138,10 +130,8 @@ int next_ticket = 0; // next ticket number to be issued
                     {
 #pragma omp critical
                         {
-                            // if (grid_queue.empty())
-                            // {
+                     
                             g = private_g;
-                            // TODO: omp cancel
                             // #pragma omp cancel parallel
                             // std::cout << "solution found!!!!" << std::endl;
                             found_solution = true;
@@ -150,9 +140,9 @@ int next_ticket = 0; // next ticket number to be issued
                     }
                     break;
 
-                    // }
+             
                 }
-                // std::cout << row << col << std::endl;
+
                 // make guesses, if it is possible, added to the queue
                 for (guess = 1; guess <= grid_size; guess++)
                 {
@@ -172,14 +162,7 @@ int next_ticket = 0; // next ticket number to be issued
                     }
                     omp_unset_lock(&queue_lock);
                 }
-                // std::cout << "tid " << tid << ": now serving: " << now_serving << "grid in queue" << grid_queue.size() << std::endl;
-
-                // for (auto &grid : grid_queue)
-                // {
-                // grid.display_values();
-                // Do something with the current element (stored in elem)
-                // }
-                // exit(1);
+              
 
                 possible_grids.clear();
             }
